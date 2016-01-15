a7e2e3fd90b4ecbebb058994ea356b94ae4a9aff
musa.zulu <zuluchs@gmail.com>
2016-01-15 16:57:16 +0200

    Added Test project with a dummy test

---
+++ source/LendingLibrary.Web.Tests/DummyTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace LendingLibrary.Web.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void Dummy_Given1_ShouldDoSomething()
+        {
+            //---------------Set up test pack-------------------
+            
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+
+            //---------------Test Result -----------------------
+             Assert.AreEqual(1,1);
+        }
+         
+    }
+}
