6ac790e942f256acd47cf2c4fe96149ceb1b79c7
musa.zulu <zuluchs@gmail.com>
2016-01-18 15:20:45 +0200

    Added TestHomeController Added test for Index

---
+++ source/LendingLibrary.Web.Tests/Controllers/TestHomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using LendingLibrary.Web.Controllers;
+using NUnit.Framework;
+
+namespace LendingLibrary.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class TestHomeController
+    {
+        [Test]
+        public void Index_ShouldReturnView()
+        {
+            //---------------Set up test pack-------------------
+            var homeController = new HomeController();
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var actual = homeController.Index() as ViewResult;
+            //---------------Test Result -----------------------
+            Assert.IsNotNull(actual);
+        }
+    }
+}
